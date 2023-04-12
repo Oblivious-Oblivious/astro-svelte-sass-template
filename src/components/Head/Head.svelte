bccc27dec81468fa584f0a26d0d3ac6a20e7fae5
Oblivious <thanos.papap42@gmail.com>
2023-04-12 19:00:36 +0300

    Abstracted head tag.

---
+++ src/components/Head/Head.svelte
@@ -0,0 +1,23 @@
+<script>
+    export let title;
+</script>
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width" />
+
+    <!-- Prevent HTTP caching -->
+    <!-- <meta http-equiv="Cache-Control" content="private, no-store" /> -->
+
+    <!-- Viewport -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <!-- Description -->
+    <meta name="description" content="Astro Svelte Sass Template" />
+
+    <!-- Page Icon -->
+    <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+
+    <!-- Title -->
+    <title>{title}</title>
+</head>
