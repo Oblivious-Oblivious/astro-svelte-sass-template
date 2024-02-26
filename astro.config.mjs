e1bd7b4759ccaa262c21540735ceaaa054357e8d
Oblivious <thanos.papap42@gmail.com>
2024-02-26 12:00:43 +0200

    Added prettier linting.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
-import { defineConfig } from "astro/config";
+import {defineConfig} from "astro/config";
 
 // https://astro.build/config
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [svelte()],
+  integrations: [svelte()],
 
-    /* NOTE Options for github pages deployment */
-    // site: "https://Oblivious-Oblivious.github.io",
-    // base: "/astro-svelte-sass-template"
+  /* NOTE Options for github pages deployment */
+  // site: "https://Oblivious-Oblivious.github.io",
+  // base: "/astro-svelte-sass-template"
 });
--- src/components/Head/Head.svelte
+++ src/components/Head/Head.svelte
@@ -1,23 +1,23 @@
 <script>
-    export let title;
+  export let title;
 </script>
 
 <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width" />
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width" />
 
-    <!-- Prevent HTTP caching -->
-    <!-- <meta http-equiv="Cache-Control" content="private, no-store" /> -->
+  <!-- Prevent HTTP caching -->
+  <!-- <meta http-equiv="Cache-Control" content="private, no-store" /> -->
 
-    <!-- Viewport -->
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <!-- Viewport -->
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-    <!-- Description -->
-    <meta name="description" content="Astro Svelte Sass Template" />
+  <!-- Description -->
+  <meta name="description" content="Astro Svelte Sass Template" />
 
-    <!-- Page Icon -->
-    <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+  <!-- Page Icon -->
+  <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
 
-    <!-- Title -->
-    <title>{title}</title>
+  <!-- Title -->
+  <title>{title}</title>
 </head>
